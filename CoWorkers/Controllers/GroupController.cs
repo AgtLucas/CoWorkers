124c1c5a80cade199955edd30463086000e20416
Lucas <metallucas.ds@gmail.com>
2013-10-20 21:06:45 -0200

    Testing...

---
+++ CoWorkers/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoWorkers.Controllers
+{
+    public class GroupController : Controller
+    {
+        //
+        // GET: /Group/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CoWorkers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoWorkers.MvcApplication" Language="C#" %>
