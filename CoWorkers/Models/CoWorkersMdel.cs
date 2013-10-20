84a70a8575d12dcddd499982047aab1b6a7a3427
Lucas <metallucas.ds@gmail.com>
2013-10-20 21:44:45 -0200

    Adicionando models

---
+++ CoWorkers/Models/CoWorkersMdel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoWorkers.Models
+{
+    public class CoWorkersMdel
+    {
+        public User User { get; set; }
+        public User Profile { get; set; }
+        public Group Group { get; set; }
+        public Post Post { get; set; }
+        public Comment Comment { get; set; }
+
+        public List<Group> Groups { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
